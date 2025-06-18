0a26a7eaa6a3855e8dcb5c66e2252db5f638d261
Aditya Kumar <mradityaroy001@gmail.com>
2025-06-18 22:53:57 +0530

    Initial commit

---
+++ src/ProductInfra/ProductInfraStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace ProductInfra
+{
+    public class ProductInfraStack : Stack
+    {
+        internal ProductInfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
